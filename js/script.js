fa9595efa9a5259e41cf9b50e35623b771e6dc3d
Mauricio Rusinque <merc2717@gmail.com>
2023-11-08 20:58:37 -0500

    Correccion de errores en el archivo js

--- js/script.js
+++ js/script.js
@@ -20,47 +20,88 @@ let gameActive = true;
 let currentPlayer = "O";
 
 // ==================== FUNCTIONS ==================== //
-function main(){
-    handStatusDisplay(CURRENT_PLAYER_TURN);
+function main() {
+    handleStatusDisplay(CURRENT_PLAYER_TURN());
     listeners();
 };
 
 main();
 
-function handStatusDisplay (message){
+function handleStatusDisplay(message) {
     STATUS_DISPLAY.innerHTML = message;
 };
 
-function listeners(){
+function listeners() {
     document.querySelector('.game-container').addEventListener('click', handleCellClick);
-    document.querySelector('game-restart').addEventListener('click', handleRestartGame);
+    document.querySelector('.game-restart').addEventListener('click', handleRestartGame);
 };
 
-function handleCellClick(clickedEvent){
+function handleCellClick(clickedEvent) {
     const clickedCell = clickedEvent.target;
-    if(clickedCell.classList.contains('game-cell')){
+    if (clickedCell.classList.contains('game-cell')) {
         const clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell)
         console.log(clickedCellIndex)
-        if(GAME_STATE[clickedCellIndex] !="" || !gameActive){
+        if (GAME_STATE[clickedCellIndex] !== "" || !gameActive) {
             return;
         };
         handleCellPlayed(clickedCell, clickedCellIndex);
         handleResultValidation();
-
     };
-    console.log(clickedCell);
 };
 
-function handleCellPlayed(clickedCell, clickedCellIndex){
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = 'O';
+    restartGameState();
+    handleStatusDisplay(CURRENT_PLAYER_TURN());
+    document.querySelectorAll('.game-cell').forEach(cell => cell.innerText = '');
+};
+
+function restartGameState() {
+    let i = GAME_STATE.length;
+    while (i--) {
+        GAME_STATE[i] = '';
+    };
+};
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
     GAME_STATE[clickedCellIndex] = currentPlayer;
     clickedCell.innerText = currentPlayer;
 };
 
-function handleResultValidation(){
+function handleResultValidation() {
     let roundWon = false;
-    for(let i = 0; i < WINNINGS.length){
-
+    for (let i = 0; i < WINNINGS.length; i++) {
+        const winCondition = WINNINGS[i];
+        let position1 = GAME_STATE[winCondition[0]];
+        let position2 = GAME_STATE[winCondition[1]];
+        let position3 = GAME_STATE[winCondition[2]];
+        if (position1 === "" || position2 === "" || position3 === "") {
+            continue;
+        };
+        if (position1 === position2 && position2 === position3) {
+            roundWon = true;
+            break;
+        };
     };
+    if (roundWon) {
+        handleStatusDisplay(WIN_MESSAGE())
+        gameActive = false;
+        return;
+    };
+
+    let roundDraw = !GAME_STATE.includes('');
+
+    if (roundDraw) {
+        handleStatusDisplay(DRAW_MESSAGE());
+        gameActive = false;
+        return;
+    };
+
+    handlePlayerChange();
 };
 
-
+function handlePlayerChange() {
+    currentPlayer = (currentPlayer === 'x') ? 'O' : 'x';
+    handleStatusDisplay(CURRENT_PLAYER_TURN());
+};
